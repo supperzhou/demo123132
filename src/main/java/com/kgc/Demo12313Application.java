2fcc66fcde75d70d9b71e7b988d891fdb0bd9d5e
supperzhou <2170597858@qq.com>
2021-10-15 16:47:25 +0800

    Initial commit

---
+++ src/main/java/com/kgc/Demo12313Application.java
@@ -0,0 +1,13 @@
+package com.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo12313Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo12313Application.class, args);
+    }
+
+}
